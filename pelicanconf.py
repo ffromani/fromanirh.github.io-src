fc53be9a31a5b9ad949a970c00eb264e5822b417
Francesco Romani <fromani@redhat.com>
2017-01-26 11:04:01 +0100

    pelican: initial configuration

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Francesco Romani'
+SITENAME = u'virt-bits'
+SITEURL = u''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('oVirt', 'http://www.ovirt.org/'),
+    ('Red Hat', 'http://www.redhat.com/'),
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+)
+
+# Social widget
+SOCIAL = (
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
